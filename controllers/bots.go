66e7e18372e1375092e58241d6d06b5ba062e144
Sameer Ali <info@iridiumsoft.org>
2018-07-10 02:50:26 +0500

    routing changes

--- controllers/bots.go
+++ controllers/bots.go
@@ -1,19 +1,19 @@
 package controllers
 
 import (
-	"github.com/robfig/cron"
 	"os"
+	"github.com/robfig/cron"
 	"github.com/sirupsen/logrus"
 )
 
 func (c *Controllers) InitBot() {
 	jobs := cron.New()
 	AppEnv := os.Getenv("APP_ENV")
-	logrus.Print(AppEnv)
-
-	jobs.AddFunc("@daily", func() {
-		if AppEnv == "production" {
+	if AppEnv == "production" {
+		logrus.Println("We are in Production Mode")
+		jobs.AddFunc("@daily", func() {
+			logrus.Println("Bot started on @Daily Basis")
 			go c.DBDump(false)
-		}
-	})
+		})
+	}
 }
